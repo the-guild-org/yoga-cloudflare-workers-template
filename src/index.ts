8a21703330c112d2efde7e7e665fa884860abae6
Charly POLY <cpoly55@gmail.com>
2022-10-17 15:00:37 +0200

    feat: update to GraphQL Yoga 3

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,67 @@
-import { createServer } from '@graphql-yoga/common'
+import { createYoga, createSchema } from "graphql-yoga";
 
+const schema = createSchema({
+  typeDefs: /* GraphQL */ `
+    type PokemonSprites {
+      front_default: String!
+      front_shiny: String!
+      front_female: String!
+      front_shiny_female: String!
+      back_default: String!
+      back_shiny: String!
+      back_female: String!
+      back_shiny_female: String!
+    }
+    type Pokemon {
+      id: ID!
+      name: String!
+      height: Int!
+      weight: Int!
+      sprites: PokemonSprites!
+    }
+    type Query {
+      pokemon(id: ID!): Pokemon
+    }
+  `,
+  resolvers: {
+    Query: {
+      pokemon: async (_parent, { id }) => {
+        const result = await fetch(
+          new Request(`https://pokeapi.co/api/v2/pokemon/${id}`),
+          {
+            // @ts-expect-error
+            cf: {
+              // Always cache this fetch regardless of content type
+              // for a max of 1 min before revalidating the resource
+              cacheTtl: 50,
+              cacheEverything: true,
+            },
+          }
+        );
+        return await result.json();
+      },
+    },
+  },
+});
 
-const server = createServer({
-  schema: {
-    typeDefs: /* GraphQL */`
-      type PokemonSprites {
-        front_default: String!
-        front_shiny: String!
-        front_female: String!
-        front_shiny_female: String!
-        back_default: String!
-        back_shiny: String!
-        back_female: String!
-        back_shiny_female: String!
-      }
-      type Pokemon {
-        id: ID!
-        name: String!
-        height: Int!
-        weight: Int!
-        sprites: PokemonSprites!
-      }
-      type Query {
-        pokemon(id: ID!): Pokemon
+const yoga = createYoga({
+  schema,
+  graphiql: {
+    defaultQuery: /* GraphQL */ `
+      query samplePokeAPIquery {
+        pokemon: pokemon(id: 1) {
+          id
+          name
+          height
+          weight
+          sprites {
+            front_shiny
+            back_shiny
+          }
+        }
       }
     `,
-    resolvers: {
-      Query: {
-        pokemon: async (_parent, { id }) => {
-          const result = await fetch(
-            new Request(`https://pokeapi.co/api/v2/pokemon/${id}`),
-            {
-              // @ts-expect-error
-              cf: {
-                // Always cache this fetch regardless of content type
-                // for a max of 1 min before revalidating the resource
-                cacheTtl: 50,
-                cacheEverything: true
-              },
-            }
-          )
-          return await result.json()
-        },
-      }
-    }
   },
-  graphiql: {
-    defaultQuery: /* GraphQL */`
-query samplePokeAPIquery {
-  pokemon: pokemon(id:1) {
-    id,
-    name,
-    height,
-    weight,
-    sprites{
-      front_shiny,
-      back_shiny
-    }
-  }
-}`
-  }
-})
+});
 
-server.start()
+self.addEventListener("fetch", yoga);
